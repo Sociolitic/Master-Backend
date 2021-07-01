c93e00f41ee7ab3a7408e40b0c0108e9773a4f33
NithinSGowda <nithin.s491@gmail.com>
2021-07-01 12:07:16 +0530

    totally working stream

--- routes/data.js
+++ routes/data.js
@@ -5,6 +5,7 @@ var request = require('request');
 // const Users = require('../models/users');
 const youtube = require('../models/youtube');
 const reddit = require('../models/redditData');
+const redditCount = require('../models/redditCount');
 
 router.get('/instagram/:query', (req, res) => {
     data = JSON.parse(JSON.stringify(req.body))
@@ -54,58 +55,115 @@ router.get('/youtube',(req,res)=>{
 
 router.get('/reddit', (req, res) => {
     q=req.query.q
+    limit=0
     d2=new Date()
-    var options = {
-        'method': 'GET',
-        'url': 'http://localhost:5000/reddit/search/?q=' + q + '&sort=hot&limit=12'  //Change to docker name
-    };
-    var stop=false
-    request(options, function (error, response) {
-        if (error){
-            throw new Error(error)
-        }else{
-            console.log("DONE");
-            stop=true
-        }
-    });
-    res.writeHead(200, {
-        'Transfer-Encoding': 'chunked',
-        'X-Content-Type-Options': 'nosniff'});
-    reddit.find({tag: q, updatedAt : { $lte : d2}}, function (err, docs){
+    redditCount.find({tag: q}, function (err, docs){
         if(err){
             throw new Error(err)
         }else{
-            if(docs[0]){
-                res.write(JSON.stringify(docs))
+            if(docs.length>0){
+                limit = docs[0].Total_reviews
+            }else{
+                limit = 5
             }
-        }
-    })
-    var done=true
-    var interval = setInterval(() => {
-        if(stop){
-            clearInterval(interval)
-            res.end()
-        }else{
-            if(done){
-                reddit.find({tag: q, updatedAt : { $gt : d2}}, function (err, docs){
-                    done=false
+            var options
+            var stop=false
+            res.writeHead(200, {
+                'Transfer-Encoding': 'chunked',
+                'X-Content-Type-Options': 'nosniff'});
+            if(req.query.more && req.query.more == 1){
+                console.log("MOREE",limit);
+                options = {
+                    'method': 'GET',
+                    'url': `http://localhost:5000/reddit/search/?q=${q}&sort=hot&limit=${limit+5}`  //Change to docker name
+                };
+                request(options, function (error, response) {
+                    if (error){
+                        throw new Error(error)
+                    }else{
+                        console.log("DONE");
+                        stop=true
+                    }
+                });
+                var done=true
+                var interval = setInterval(() => {
+                    if(stop){
+                        clearInterval(interval)
+                        res.end()
+                    }else{
+                        if(done){
+                            reddit.find({tag: q, updatedAt : { $gt : d2}}, function (err, docs){
+                                done=false
+                                if(err){
+                                    throw new Error(err)
+                                }else{
+                                    done=true
+                                    if(docs[0]){
+                                        res.write(JSON.stringify(docs))
+                                        d2=docs[docs.length-1].updatedAt
+                                        for(e of docs){
+                                            console.log(e.title);
+                                        }
+                                    }
+                                }
+                            })
+                        }
+                    }
+                }, 1000);
+            }else{
+                console.log("PREVV");
+                options = {
+                    'method': 'GET',
+                    'url': `http://localhost:5000/reddit/search/?q=${q}&sort=hot&limit=${limit}`  //Change to docker name
+                };
+                request(options, function (error, response) {
+                    if (error){
+                        throw new Error(error)
+                    }else{
+                        console.log("DONE");
+                        stop=true
+                    }
+                });
+                
+                reddit.find({tag: q, updatedAt : { $lte : d2}}, function (err, docs){
                     if(err){
                         throw new Error(err)
                     }else{
-                        done=true
                         if(docs[0]){
                             res.write(JSON.stringify(docs))
-                            d2=docs[docs.length-1].updatedAt
-                            console.log(d2);
-                            for(e of docs){
-                                console.log(e.title);
-                            }
                         }
                     }
                 })
+                var done=true
+                var interval = setInterval(() => {
+                    if(stop){
+                        clearInterval(interval)
+                        res.end()
+                    }else{
+                        if(done){
+                            reddit.find({tag: q, updatedAt : { $gt : d2}}, function (err, docs){
+                                done=false
+                                if(err){
+                                    throw new Error(err)
+                                }else{
+                                    done=true
+                                    if(docs[0]){
+                                        res.write(JSON.stringify(docs))
+                                        d2=docs[docs.length-1].updatedAt
+                                        for(e of docs){
+                                            console.log(e.title);
+                                        }
+                                    }
+                                }
+                            })
+                        }
+                    }
+                }, 1000);
             }
+           
+            
         }
-    }, 1000);
+    })
 })
 
 module.exports = router;
